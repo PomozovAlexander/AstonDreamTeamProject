febe2e5c71be8a5f01ed2029028f3c13c6e52abb
wlood <61375455+wloodheart@users.noreply.github.com>
2025-02-12 01:41:38 +0300

    service update (added javadoc, searching method, output methods)

--- src/main/java/ru/aston/shellsorter/service/BookService.java
+++ src/main/java/ru/aston/shellsorter/service/BookService.java
@@ -1,32 +1,56 @@
 package ru.aston.shellsorter.service;
 
 import ru.aston.shellsorter.model.Book;
+import ru.aston.shellsorter.utils.cli.BookArrayCLIBuilder;
 import ru.aston.shellsorter.utils.generator.BookRandomGenerator;
 
 import java.util.Arrays;
+import java.util.Optional;
 import java.util.Random;
 
+/**
+ * Service implementation for operations on an array of {@link Book} objects.
+ */
 public class BookService implements Service {
     private Book[] array;
     private boolean sorted = false;
     private String sortedField = "Author"; //default field
 
+    /**
+     * Fills the array with randomly generated Book objects.
+     *
+     * @param length the number of Books to generate.
+     */
     @Override
     public void randomGeneratedFill(int length) {
         array = new BookRandomGenerator(new Random()).generateArray(length);
         System.out.println(Arrays.toString(array));
     }
 
+    /**
+     * Fills the array with Books from a file.
+     *
+     * @param length the number of Books to fill.
+     */
     @Override
     public void fromFileFill(int length) {
 
     }
 
+    /**
+     * Fills the array via CLI.
+     *
+     * @param length the number of Books to fill.
+     */
     @Override
     public void manualFill(int length) {
-
+        array = BookArrayCLIBuilder.buildBookArrayFromCLI(length);
+        System.out.println(Arrays.toString(array));
     }
 
+    /**
+     * Sorting the array by default comparator.
+     */
     @Override
     public void sort() {
 
@@ -36,6 +60,11 @@ public class BookService implements Service {
         sorted = true;
     }
 
+    /**
+     * Sorts the array based on the specified field.
+     *
+     * @param field the name of the field to sort by.
+     */
     @Override
     public void sortByField(String field) {
 
@@ -46,11 +75,56 @@ public class BookService implements Service {
         sortedField = field;
     }
 
+    /**
+     * Searching in the array for the specified query.
+     *
+     * @param request the search query string.
+     */
+    @Override
+    public void search(String request) {
+        Optional<Book> searchingResult = Optional.empty(); //внести результат поиска
+
+        //todo вызвать поиск по полю <sortedField>
+
+        searchingResult.ifPresentOrElse(
+                System.out::println,
+                () -> System.out.printf("Nothing found for your request %s%n.", request)
+        );
+    }
+
+    /**
+     * Printing an array to CLI.
+     */
+    @Override
+    public void print() {
+        System.out.println(Arrays.toString(array));
+    }
+
+    /**
+     * Saving an array to file.
+     */
+    @Override
+    public void save() {
+
+        //TODO: Реализовать сохранение в файл
+
+    }
+
+    /**
+     * Returns the name of the field by which the array has been sorted.
+     *
+     * @return the name of the sorted field, or a default value.
+     */
     @Override
     public String getSortedField() {
         return sortedField;
     }
 
+    /**
+     * Checks whether the collection or data structure is sorted.
+     *
+     * @return {@code true} if the data is sorted, otherwise {@code false}.
+     */
     @Override
     public boolean isSorted() {
         return sorted;
--- src/main/java/ru/aston/shellsorter/service/RootVegetableService.java
+++ src/main/java/ru/aston/shellsorter/service/RootVegetableService.java
@@ -1,32 +1,56 @@
 package ru.aston.shellsorter.service;
 
 import ru.aston.shellsorter.model.RootVegetable;
+import ru.aston.shellsorter.utils.cli.RootVegetableArrayCLIBuilder;
 import ru.aston.shellsorter.utils.generator.RootVegetableRandomGenerator;
 
 import java.util.Arrays;
+import java.util.Optional;
 import java.util.Random;
 
+/**
+ * Service implementation for operations on an array of {@link RootVegetable} objects.
+ */
 public class RootVegetableService implements Service {
     private RootVegetable[] array;
     private boolean sorted = false;
     private String sortedField = "Weight"; //default field
 
+    /**
+     * Fills the array with randomly generated RootVegetable objects.
+     *
+     * @param length the number of RootVegetables to generate.
+     */
     @Override
     public void randomGeneratedFill(int length) {
         array = new RootVegetableRandomGenerator(new Random()).generateArray(length);
         System.out.println(Arrays.toString(array));
     }
 
+    /**
+     * Fills the array with RootVegetables from a file.
+     *
+     * @param length the number of RootVegetables to fill.
+     */
     @Override
     public void fromFileFill(int length) {
 
     }
 
+    /**
+     * Fills the array via CLI.
+     *
+     * @param length the number of RootVegetables to fill.
+     */
     @Override
     public void manualFill(int length) {
-
+        array = RootVegetableArrayCLIBuilder.buildRootVegetableArrayFromCLI(length);
+        System.out.println(Arrays.toString(array));
     }
 
+    /**
+     * Sorting the array by default comparator.
+     */
     @Override
     public void sort() {
 
@@ -36,6 +60,11 @@ public class RootVegetableService implements Service {
         sorted = true;
     }
 
+    /**
+     * Sorts the array based on the specified field.
+     *
+     * @param field the name of the field to sort by.
+     */
     @Override
     public void sortByField(String field) {
 
@@ -46,11 +75,56 @@ public class RootVegetableService implements Service {
         sortedField = field;
     }
 
+    /**
+     * Searching in the array for the specified query.
+     *
+     * @param request the search query string.
+     */
+    @Override
+    public void search(String request) {
+        Optional<RootVegetable> searchingResult = Optional.empty(); //внести результат поиска
+
+        //todo вызвать поиск по полю <sortedField>
+
+        searchingResult.ifPresentOrElse(
+                System.out::println,
+                () -> System.out.printf("Nothing found for your request %s%n.", request)
+        );
+    }
+
+    /**
+     * Printing an array to CLI.
+     */
+    @Override
+    public void print() {
+        System.out.println(Arrays.toString(array));
+    }
+
+    /**
+     * Saving an array to file.
+     */
+    @Override
+    public void save() {
+
+        //TODO: Реализовать сохранение в файл
+
+    }
+
+    /**
+     * Returns the name of the field by which the array has been sorted.
+     *
+     * @return the name of the sorted field, or a default value.
+     */
     @Override
     public String getSortedField() {
         return sortedField;
     }
 
+    /**
+     * Checks whether the collection or data structure is sorted.
+     *
+     * @return {@code true} if the data is sorted, otherwise {@code false}.
+     */
     @Override
     public boolean isSorted() {
         return sorted;
